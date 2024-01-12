1d82991dc83e69fa56b46b516994cedc3f2f697b
Antoine Bauchot <bchtantoine@gmail.com>
2024-01-12 14:43:40 +0100

    pwa install

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
 import { Inter } from 'next/font/google'
+import { Metadata } from 'next'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
+  manifest: '/manifest.json',
   title: 'Campus Quest',
   description: 'A quest to find the best campus in the world',
 }
