5ca1b869b5f5b4c19b13453bac120aa243ff3486
Antoine Bauchot <bchtantoine@gmail.com>
2024-01-29 16:20:43 +0100

    QRCode redirection

--- src/app/qrscan/page.js
+++ src/app/qrscan/page.js
@@ -15,6 +15,13 @@ const QRScanPage = () => {
         const onScanSuccess = (decodedText, decodedResult) => {
             console.log(`Code scanned = ${decodedText}`, decodedResult);
             // Handle the scanned text as needed.
+            if (decodedText.startsWith('http://') || decodedText.startsWith('https://')) {
+                // If the decoded text is a URL, navigate to it
+                window.location.href = decodedText;
+            } else {
+                // Otherwise, handle the text as needed
+                console.log(`QR code does not contain a URL: ${decodedText}`);
+            }
         };
         html5QrCode.render(onScanSuccess, (errorMessage) => {
             console.log(errorMessage);
@@ -26,8 +33,9 @@ const QRScanPage = () => {
     }, []);
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
-            <div id="qr-reader" ref={qrRef} className="w-full max-w-md"></div>
+        <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-black">
+            <div id="qr-reader" ref={qrRef} className="w-full max-w-md">
+            </div>
         </div>
     );
 };
