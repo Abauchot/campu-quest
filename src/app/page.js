2b50aac31cb6e219c47907b73caeb10d7b95992d
Antoine Bauchot <bchtantoine@gmail.com>
2024-02-24 15:11:07 +0100

    switch english

--- src/app/page.js
+++ src/app/page.js
@@ -6,27 +6,28 @@ export default function Home() {
       <Navbar />
       <div className="bg-black min-h-screen flex flex-col items-center justify-center">
         <div className="max-w-4xl mx-auto p-8">
-          <h1 className="text-4xl font-bold text-center mb-4">Bienvenue sur CampusQuest!</h1>
+          <h1 className="text-4xl font-bold text-center mb-4">Welcome to CampusQuest!</h1>
           <p className="text-xl text-center mb-8">
-            Partez à l'aventure dans MyDigitalSchool et decouvrez les secrets caches à travers des enigmes captivantes. Trouvez les QR codes dissimules pour avancer dans votre quete.
+            Embark on an adventure at MyDigitalSchool and uncover hidden secrets through captivating puzzles. Find the hidden QR codes to progress in your quest.
           </p>
           <div className="flex flex-col items-center">
-            <h2 className="text-3xl font-bold mb-3">Comment jouer?</h2>
+            <h2 className="text-3xl font-bold mb-3">How to Play?</h2>
             <ol className="list-decimal list-inside text-lg mb-8">
-              <li>Demarrez votre quête à l'accueil du campus.</li>
-              <li>Resolvez les enigmes pour trouver l'emplacement des QR codes.</li>
-              <li>Scannez le QR code avec votre appareil pour recevoir votre prochain indice.</li>
-              <li>Continuez jusqu'à resoudre toutes les enigmes et devoiler le tresor cache.</li>
+              <li>Start your quest at the campus reception.</li>
+              <li>Solve puzzles to find the location of the QR codes.</li>
+              <li>Scan the QR code with your device to receive your next clue.</li>
+              <li>Continue until you solve all puzzles and uncover the hidden treasure.</li>
             </ol>
             <a
               href="/quests"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
-              Commencez votre quête maintenant!
+              Start your quest now!
             </a>
           </div>
         </div>
       </div>
     </div>
+
   );
 }
