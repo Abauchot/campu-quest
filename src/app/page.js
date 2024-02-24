a5629afbe2d1d09e8ef2b636749884bbc12da496
Antoine Bauchot <bchtantoine@gmail.com>
2024-02-24 15:08:56 +0100

    remove é

--- src/app/page.js
+++ src/app/page.js
@@ -8,15 +8,15 @@ export default function Home() {
         <div className="max-w-4xl mx-auto p-8">
           <h1 className="text-4xl font-bold text-center mb-4">Bienvenue sur CampusQuest!</h1>
           <p className="text-xl text-center mb-8">
-            Partez à l'aventure dans MyDigitalSchool et découvrez les secrets cachés à travers des énigmes captivantes. Trouvez les QR codes dissimulés pour avancer dans votre quête.
+            Partez à l'aventure dans MyDigitalSchool et decouvrez les secrets caches à travers des enigmes captivantes. Trouvez les QR codes dissimules pour avancer dans votre quete.
           </p>
           <div className="flex flex-col items-center">
             <h2 className="text-3xl font-bold mb-3">Comment jouer?</h2>
             <ol className="list-decimal list-inside text-lg mb-8">
-              <li>Démarrez votre quête à l'accueil du campus.</li>
-              <li>Résolvez les énigmes pour trouver l'emplacement des QR codes.</li>
+              <li>Demarrez votre quête à l'accueil du campus.</li>
+              <li>Resolvez les enigmes pour trouver l'emplacement des QR codes.</li>
               <li>Scannez le QR code avec votre appareil pour recevoir votre prochain indice.</li>
-              <li>Continuez jusqu'à résoudre toutes les énigmes et dévoiler le trésor caché.</li>
+              <li>Continuez jusqu'à resoudre toutes les enigmes et devoiler le tresor cache.</li>
             </ol>
             <a
               href="/quests"
